ef7122582ae902042603d273f10589dbad1de5ec
Satyam Seth <satyam1998.1998@gmail.com>
2024-10-28 21:34:44 +0530

    format changes

--- mera_gurukul/wsgi.py
+++ mera_gurukul/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mera_gurukul.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mera_gurukul.settings")
 
 application = get_wsgi_application()
